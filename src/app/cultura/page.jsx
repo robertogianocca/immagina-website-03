b4d27ed35fe58a4403b499890d0ffc50b12f6263
Roberto <robertogianocca@outlook.com>
2025-03-08 15:55:09 +0100

    Restructure intro section. Add grey background to the portfolio section.

--- src/app/cultura/page.jsx
+++ src/app/cultura/page.jsx
@@ -2,9 +2,7 @@
 
 import fetchPortfolioData from "@/utils/fetchPortfolioData";
 import NavigationBar from "@/components/NavigationBar/NavigationBar";
-import Wrapper from "@/components/Wrapper/Wrapper";
 import IntroSection from "@/components/Sections/IntroSection/IntroSection";
-// import PortfolioSection from "@/components/Sections/PortfolioSection/PortfolioSection";
 import TeamSection from "@/components/Sections/TeamSection/TeamSection";
 import PortfolioCategoryCard from "@/components/Portfolio/PortfolioCategoryCard/PortfolioCategoryCard";
 
@@ -25,9 +23,9 @@ export default async function Cultura() {
       <NavigationBar
         color="text-custom-red"
         menuColor="text-custom-red hover:border-b-2 hover:border-custom-red"
-        bgColor="bg-customWhite lg:shadow-xl lg:shadow-slate-200"
+        bgColor="bg-custom-white lg:shadow-xl lg:shadow-slate-200"
       />
-      <main className="min-h-[calc(100vh-60px)] w-full mt-[60px]  pb-20 bg-custom-grey">
+      <main className="min-h-[calc(100vh-60px)] w-full mt-[60px] pb-20 bg-custom-grey">
         {/* ---------- INTRO SECTION ---------- */}
         <section
           id="home"
@@ -35,31 +33,32 @@ export default async function Cultura() {
         >
           <IntroSection
             text={
-              <>
-                <p className="font-semibold text-xs md:text-base">
+              <div className="flex flex-col gap-y-4 mb-4 font-semibold text-xs xl:text-base">
+                <p>
                   Questa sezione del sito di IMMAGINA è dedicata agli
                   <span className="font-bold text-custom-brown-red"> artisti</span> e agli
                   <span className="font-bold text-custom-brown-red"> operatori culturali</span>.
-                  <br />
-                  <br />
-                  {`Come il sarto, vestiamo su misura ogni aspetto legato alla sconfinata attività culturale. Per mezzo di foto, video, testi.  Seguiamo la produzione di libri, cataloghi, manifesti, mostre… `}
-                  <br />
-                  <br />
-                  {`La nostra missione: Vestire la cultura, comunicare con successo il profumo dell’arte.`}
-                  <br />
-                  <br />
+                </p>
+                <p>
+                  Come il sarto, vestiamo su misura ogni aspetto legato alla sconfinata attività
+                  culturale. Per mezzo di foto, video, testi. Seguiamo la produzione di libri,
+                  cataloghi, manifesti, mostre…
+                </p>
+                <p>
+                  La nostra missione: Vestire la cultura, comunicare con successo il profumo
+                  dell’arte.
                 </p>
                 <p className="hidden sm:block font-semibold">
                   IMMAGINA ti invita a <span className="italic">sfogliare</span> il suo portfolio.
                 </p>
-              </>
+              </div>
             }
           />
         </section>
         {/* ---------- PORTFOLIO SECTION ---------- */}
         <section
           id="portfolio"
-          className="h-space lg:min-h-[calc(100vh-60px)] pt-4 xl:pt-8 px-4 lg:px-6 xl:pl-14 xl:pr-24 pb-20"
+          className="h-space lg:min-h-[calc(100vh-60px)] pt-4 xl:pt-8 px-4 lg:px-6 xl:pl-14 xl:pr-24 pb-20 bg-zinc-300"
         >
           <h2 className="text-3xl xl:text-4xl font-courier font-bold mb-6">Portfolio</h2>
           <div className="main-grid">
--- src/components/Sections/IntroSection/IntroSection.jsx
+++ src/components/Sections/IntroSection/IntroSection.jsx
@@ -6,45 +6,15 @@ import Triangle from "@/components/Icons/Triangle";
 
 export default function IntroSection({ text }) {
   return (
-    <div className="h-full sm:h-auto flex flex-col justify-between sm:justify-no sm:grid sm:grid-cols-3 sm:gap-4 lg:gap-x-8 pb-6 lg:pb-[30px] font-bold">
-      <div className="flex flex-col gap-[3vh] lg:gap-x-10 xl:gap-y-[2vh] sm:col-span-2 lg:col-span-1 text-sm xl:text-base">
+    <div className="h-full sm:h-auto flex flex-col justify-between sm:justify-no sm:grid sm:grid-cols-3 sm:gap-4 xl:gap-x-8 pb-6 lg:pb-[30px] font-bold">
+      <div className="flex flex-col sm:col-span-2 lg:col-span-1 text-sm xl:text-base">
         {text}
-        <Link href="#portfolio" className="">
+        <Link href="#portfolio">
           <RedButton addClass="p-2 px-3 text-base xl:text-xl font-courier mb-6 lg:font-light">
             Portfolio
           </RedButton>
         </Link>
-        {/* ---------- PORTFOLIO LIST ---------- */}
-        {/* <ul className="grid grid-cols-2 sm:flex lg:grid gap-x-6 gap-y-4 2xl:gap-x-[min(1rem)] 2xl:w-[max(400px)] xs:pr-4 xl:pr-0 text-center lg:mb-0 font-courier text-base xl:text-lg mb-4">
-          <li className="flex">
-            <Triangle addClass="border-l-red-600" />
-            <Link href="/cultura/photography" className="hover:text-customRed">
-              Photography
-            </Link>
-          </li>
-          <Link href="/cultura/video" className="hover:text-customRed">
-            <li className="flex items-center">
-              <Triangle addClass="border-l-customRed" />
-              Video
-            </li>
-          </Link> */}
-        {/* <Link href="/grafica" className="hover:text-customRed">
-            <li className="flex items-centers">
-              <Triangle addClass="border-l-customRed" />
-              Graphic
-            </li>
-          </Link>
-          <Link href="/video" className="hover:text-customRed">
-            <li className="flex items-center">
-              <Triangle addClass="border-l-customRed" />
-              Bookdesign
-            </li>
-          </Link> */}
-        {/* </ul> */}
-        {/* ---------- IF YOU LIKE IT ---------- */}
-        {/* <p className="mb-4 font-semibold">If you like it, scrivici. Why not.</p> */}
       </div>
-
       {/*  ------------ VIDEO ------------ */}
       <video
         preload="auto"
