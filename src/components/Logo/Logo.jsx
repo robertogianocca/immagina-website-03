18bd41cffc0fb6ad03a4980d568b816925beaf61
Roberto <robertogianocca@outlook.com>
2025-03-20 13:40:20 +0100

    Try fix layout shift.

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,5 +1,9 @@
 import Image from "next/image";
 
 export default function Logo({ logo }) {
-  return <Image src={logo} width={190} height={190} alt="Logo IMMAGINA" loading="eager" />;
+  return (
+    <div className="w-[190px] h-auto">
+      <Image src={logo} width={190} height={190} alt="Logo IMMAGINA" priority />
+    </div>
+  );
 }
--- src/components/NavigationBar/NavigationBar.jsx
+++ src/components/NavigationBar/NavigationBar.jsx
@@ -12,14 +12,14 @@ export default function NavigationBar({ color, menuColor, bgColor }) {
       className={`w-full h-[60px] fixed left-0 top-0 main-grid flex-row justify-between px-4 lg:px-6 xl:pl-14 xl:pr-24 z-50 bg-custom-white`}
     >
       {/* ---------- LOGO ---------- */}
-      <div className="w-[190px] pt-2">
+      <div className="pt-2">
         <Link href="/cultura">
           <Logo logo={logoRed} />
           <p className="text-custom-red font-courier font-bold">cultura</p>
         </Link>
       </div>
       {/* ---------- MENU ---------- */}
-      <div className="flex flex-row items-center lg:col-span-2 justify-between">
+      <div className="w-full h-auto flex flex-row items-center lg:col-span-2 justify-between ">
         <div>
           <MenuDesktop menuItems={menuItems} menuColor={menuColor} />
         </div>
