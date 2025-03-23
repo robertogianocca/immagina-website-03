2d6d1252e79f5d54132bae9fe5cfd9f8a946739d
Roberto <robertogianocca@outlook.com>
2025-03-23 18:19:25 +0100

    Add contact on mobile.

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Logo({ logo }) {
   return (
-    <div className="w-[190px] h-auto">
+    <div className="w-[140px] sm:w-[190px] h-auto">
       <Image src={logo} width={190} height={60} alt="IMMAGINA" priority={true} />
     </div>
   );
--- src/components/NavigationBar/NavigationBar.jsx
+++ src/components/NavigationBar/NavigationBar.jsx
@@ -9,21 +9,21 @@ const menuItems = ["Portfolio", "Team"];
 export default function NavigationBar({ color, menuColor, bgColor }) {
   return (
     <nav
-      className={`w-full h-[60px] fixed left-0 top-0 main-grid justify-between px-4 lg:px-6 xl:pl-14 xl:pr-24 z-50 overflow-hidden bg-custom-white`}
+      className={`w-full h-[60px] fixed left-0 top-0 flex-row main-grid justify-between px-4 lg:px-6 xl:pl-14 xl:pr-24 z-50 overflow-hidden bg-custom-white`}
     >
       {/* ---------- LOGO ---------- */}
-      <div className="pt-2">
+      <div className="pt-3 sm:pt-2">
         <Link href="/cultura">
           <Logo logo={logoRed} />
           <p className="text-custom-red font-courier font-bold">cultura</p>
         </Link>
       </div>
       {/* ---------- MENU ---------- */}
-      <div className="w-full h-auto flex flex-row items-center lg:col-span-2 justify-between ">
+      <div className="w-full h-auto flex flex-row items-center lg:col-span-2 justify-between  ">
         <div>
           <MenuDesktop menuItems={menuItems} menuColor={menuColor} />
         </div>
-        <p className="text-xs md:text-sm pl-4 xl:pl-0 font-bold text-custom-red">
+        <p className="text-2xs sm:text-xs md:text-sm mt-[-6px] sm:mt-0 pl-4 xl:pl-0 font-bold text-custom-red">
           <Link href="mailto:contact@immagina.ch">contact@immagina.ch</Link>
         </p>
       </div>
