ad59ba819425495229b71701f10f249dbc3781c6
Roberto <robertogianocca@outlook.com>
2025-11-04 09:59:53 +0100

    Hide metadata video.

--- src/app/cultura/video/page.jsx
+++ src/app/cultura/video/page.jsx
@@ -2,10 +2,10 @@
 
 import VideoContainer from "@/components/Portfolio/VideoContainer/VideoContainer";
 
-export const metadata = {
-  description:
-    "IMMAGINA offre servizi video a tutto tondo. Produciamo di preferenza film per artisti, musicisti, attori, teatri, festival, matrimoni.",
-};
+// export const metadata = {
+//   description:
+//     "IMMAGINA offre servizi video a tutto tondo. Realizziamo film indipendenti, videoclip per musicisti, teatri, festival, matrimoni.",
+// };
 
 export default function VideoPage() {
   return <VideoContainer />;
